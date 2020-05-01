2a2697672fa9838bff34ac471e72960b0db57ec4
Warchiefs <ialexmur@gmail.com>
2020-05-01 14:18:20 +0300

    refact keyword validation

--- src/Schema/Keywords/Nullable.php
+++ src/Schema/Keywords/Nullable.php
@@ -12,12 +12,23 @@ class Nullable extends BaseKeyword
      * Allows sending a null value for the defined schema. Default value is false.
      *
      * @param mixed $data
+     * @param bool $nullable
+     * @param string|array $type
      *
      * @throws KeywordMismatch
      */
-    public function validate($data, bool $nullable) : void
+    public function validate($data, bool $nullable, $type) : void
     {
-        if (! $nullable && ($data === null)) {
+        if ($data === null) {
+            if (is_array($type) && (array_search('null', $type) || array_search(null, $type))) {
+                return;
+            }
+            if ($type === null || $type === 'null') {
+                return;
+            }
+            if ($nullable) {
+                return;
+            }
             throw KeywordMismatch::fromKeyword('nullable', $data, 'Value cannot be null');
         }
     }
